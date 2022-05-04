1e31cd841c608a46ec083848662ee3699ed888fe
Chan <lhc0506@gmail.com>
2022-05-04 18:37:56 +0900

    feature: make draggable component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('renders learn react link', () => {
+  it('should mark items as draggable', function () {
+    render(<App />);
+    const draggableElement = screen.getByTestId("draggable");
+    expect(draggableElement.getAttribute('draggable')).toBeTruthy();
+  });
 });
